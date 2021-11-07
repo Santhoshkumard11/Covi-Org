9c63259b39ed422070318b71987590150899c940
Santhoshkumard11 <santhoshkdhana@gmail.com>
2021-11-07 18:33:32 +0530

    working department graph generaton

--- generate_graphs/generate_graph.py
+++ generate_graphs/generate_graph.py
@@ -1,7 +1,9 @@
+from typing import Tuple
 import pandas as pd
 from blob_storage_client import BlobStorageClient
 import logging
 import io
+import matplotlib.pyplot as plt
 
 
 class GenerateGraph:
@@ -16,7 +18,12 @@ class GenerateGraph:
         self.blob_client_obj_vaccine_name = BlobStorageClient(
             "coviorg-graph", "vaccine_name_graph.png")
 
+        self.blob_client_obj_department_vaccination_status = BlobStorageClient(
+            "coviorg-graph", "department_vaccination_status.png")
+
         self.vaccine_status_df, self.vaccine_name_df = None, None
+
+        plt.style.use('fivethirtyeight')
 
     def query_all_item_from_cosmos_db(self):
 
@@ -29,8 +36,7 @@ class GenerateGraph:
         for item in self.query_result:
 
             vaccine_name_list.append(item["vaccine_name"])
-            vaccine_status_list.append(item["is_vaccinated_status"])
-
+            vaccine_status_list.append(item["vaccination_status"])
 
         # create a new dataframe with the items from cosmos db
         df = pd.DataFrame(
@@ -46,39 +52,6 @@ class GenerateGraph:
 
         logging.info("Gathered the data and created the dataframe")
 
-    def generate_graph_vaccine_status(self):
-
-        # create pie graph with pandas dataframe
-        vaccine_status_fig = self.vaccine_status_df.plot.pie(
-            y="vaccination_status", figsize=(5, 5),
-            legend="vaccination_status", title="By Vaccination Status",
-            autopct=lambda p: '{:.0f}'.format(
-                (p / 100) * self.vaccine_status_df.sum()),
-            cmap="spring")
-
-        image_object = vaccine_status_fig.get_figure()
-
-        self.save_image_to_blob_storage(self.blob_client_obj_vaccine_status, image_object)
-
-        # clear the image object after uploading the image to blob storage
-        image_object.clear(True)
-
-    def generate_graph_vaccination_name(self):
-        # create pie graph with pandas dataframe
-        vaccine_name_fig = self.vaccine_name_df.plot.pie(
-            y="vaccine_name", figsize=(5, 5),
-            legend="vaccine_name", title="By Vaccine Name",
-            autopct=lambda p: '{:.0f}'.format(
-                (p / 100) * self.vaccine_name_df.sum()),
-            cmap="winter")
-
-        # get the figure object
-        image_object = vaccine_name_fig.get_figure()
-
-        self.save_image_to_blob_storage(self.blob_client_obj_vaccine_name, image_object)
-
-        image_object.clear(True)
-
     def save_image_to_blob_storage(self, blob_client, image_object):
         buf = io.BytesIO()
         image_object.savefig(buf, format='png')
@@ -90,6 +63,55 @@ class GenerateGraph:
         buf.flush()
         buf.close()
 
+    def generate_graph_vaccine_status(self):
+
+        # create pie graph with pandas dataframe
+        vaccine_status_fig = self.vaccine_status_df.plot.pie(
+            y="vaccination_status", figsize=(5, 5),
+            legend="vaccination_status", title="By Vaccination Status",
+            autopct=lambda p: '{:.0f}'.format(
+                (p / 100) * self.vaccine_status_df.sum()),
+            cmap="terrain")
+
+        image_object = vaccine_status_fig.get_figure()
+
+        self.save_image_to_blob_storage(
+            self.blob_client_obj_vaccine_status.blob_client, image_object)
+
+        # clear the image object after uploading the image to blob storage
+        image_object.clear(True)
+
+    def generate_graph_vaccination_name(self):
+        # create pie graph with pandas dataframe
+        vaccine_name_fig = self.vaccine_name_df.plot.pie(
+            y="vaccine_name", figsize=(5, 5),
+            legend="vaccine_name", title="By Vaccine Name",
+            autopct=lambda p: '{:.0f}'.format(
+                (p / 100) * self.vaccine_name_df.sum()),
+            cmap="plasma")
+
+        # get the figure object
+        image_object = vaccine_name_fig.get_figure()
+
+        self.save_image_to_blob_storage(
+            self.blob_client_obj_vaccine_name.blob_client, image_object)
+
+        image_object.clear(True)
+
+    def generate_graph_department_with_vaccination_status(self):
+        df_temp = pd.DataFrame(self.query_result)
+
+        department_vaccination_status_fig = df_temp.plot.scatter(
+            x="department", y="vaccination_status", color="#E26A2C")
+
+        image_object = department_vaccination_status_fig.get_figure()
+
+        self.save_image_to_blob_storage(
+            self.blob_client_obj_department_vaccination_status.blob_client,
+            image_object)
+
+        image_object.clear(True)
+
     def start_process(self):
         logging.info("Starting generate graph process")
 
@@ -97,6 +119,7 @@ class GenerateGraph:
 
         self.prepare_df()
 
+        self.generate_graph_department_with_vaccination_status()
         self.generate_graph_vaccination_name()
         self.generate_graph_vaccine_status()
 
